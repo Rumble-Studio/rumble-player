77650506410014e065f39d4a24b2ad54b735e80d
ismael-su <ismael@rumble.studio>
2021-04-22 07:06:29 +0000

    WIP Fix playlist

--- apps/demo-angular/src/config/dummyAudioData.config.ts
+++ apps/demo-angular/src/config/dummyAudioData.config.ts
@@ -25,7 +25,7 @@ export const fakePlaylistWithImage = [
 	},
 	{
 		file:
-			'https://firebasestorage.googleapis.com/v0/b/rumble-studio-alpha.appspot.com/o/assets%2Faudio%2Fjingles%2Fuplifting%2FUplifting-1.mp3?alt=media&token=60b80935-4468-425a-9a1a-bf84d443d2e2',
+			'https://firebasestorage.googleapis.com/v0/b/rumble-studio-alpha.appspot.com/o/assets%2Faudio%2Fjingles%2Fclassical%2FClassical-2.mp3?alt=media&token=1192b959-52da-4bfa-8345-ae51cd5d2f15',
 		image:
 			'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/YouTube_Red_Diamond_Play_Button.svg/1024px-YouTube_Red_Diamond_Play_Button.svg.png',
 	},
